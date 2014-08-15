9fca953e6f4a5f3b4fa9364c2b7b2140440224d3
Ryan <cumt.xiaochi@gmail.com>
2014-08-15 14:58:01 +0800

    wow

--- phpsh.php
+++ phpsh.php
@@ -1,20 +1,67 @@
 <?php
 
-// while($str = fread(STDIN,1000)){
-//     echo "$str";
-//     if ($str) {
-//         $rs = eval($str);
-//         echo "$rs\n";
-//     }
-// }
-
-function is_expression($code)
+function trim_backslash(&$code)
 {
-    return preg_match('/^\$?\w+$|^[\d.e]$/i', $code);
+    $rs = strpos($code, '\\') !== false;
+    $code = preg_replace('/\\\\./', '', $code);
+    return $rs;
 }
 
-function eval_expr($code) {
+function trim_string(&$code) {
+    $rs = strpos($code, '"') !== false || strpos($code, "'") !== false;
+    $code = preg_replace('/"[^"]*?"|'."'[^']*?'".'/', 'X', $code);
+    return $rs;
+}
+function trim_params(&$code) {
+    $rs = strpos($code, ',') !== false;
+    $code = preg_replace('/(\$?\w+|^[\d.e]+)(,\s*(\$?\w+|^[\d.e]+))+/', 'X', $code);
+    return $rs;
+}
+function trim_function(&$code) {
+    $changed = false;
+    $code = preg_replace_callback('/(\w+)\s*\(\s*(\$?\w+|^[\d.e]+)\s*\)/', function ($m) use (&$changed) {
+        if (in_array($m[1], array('if', 'while', 'do', 'for'))) {
+
+        } else {
+            $changed = true;
+        }
+    }, $code);
+    return $changed;
+}
+function is_assign($code)
+{
+    return strpos($code, '=') !== false && strpos($code, '==') === false;
+}
+
+function formulize($code) {
+    while (true) {
+        $changed = false;
+        $changed |= trim_backslash($code);
+        $changed |= trim_string($code);
+        $changed |= trim_params($code);
+        $changed |= trim_function($code);
+        // $changed |= trim_expression($code);
+        if (!$changed) {
+            break;
+        }
+    }
+    return $code;
+}
+function is_expression($code)
+{
+    $code = formulize($code);
+    return (count(explode(';', $code)) === 1 && !is_assign($code));
+    return preg_match('/^\$?\w+$|^[\d.e]+$/i', $code);
+}
+
+function complete_expr($code) {
     $code = trim($code);
+    if (empty($code)) {
+        return array(null, null);
+    }
+    if (in_array($code, array('quit'))) {
+        return array($code, null);
+    }
     $last_char = $code[strlen($code)-1];
     if (is_expression($code)) {
         $code = '$__rs = '.$code;
@@ -23,19 +70,30 @@ function eval_expr($code) {
         $code .= ';';
     }
     // var_dump($code);
-    eval($code);
-    if (isset($__rs)) {
-        return $__rs;
+    return array(null, $code);
+}
+
+function execute_command($cmd)
+{
+    if ($cmd === 'quit') {
+        exit();
     }
 }
 
-function input_and_eval()
-{
+while (true) {
     echo "phpsh > ";
     $str = fread(STDIN,1000);
-    $rs = eval_expr($str);
-    echo "$rs\n";
+    if (empty($str)) {
+        continue;
+    }
+    list($cmd, $code) = complete_expr($str);
+    if ($cmd) {
+        execute_command($cmd);
+    } elseif ($code) {
+        eval($code);
+    }
+    if (isset($__rs)) {
+        echo "$__rs\n";
+        unset($__rs);
+    }
 }
-// eval_expr('function a(){return 1;}');
-// echo $__rs;
-input_and_eval();
